5a46aa50faa07ff4bbbeb73d5b0a1ffec33cdb0f
kjkj <asdjsdjngfdsikjnhfdsijngfds.bg>
2019-03-01 15:56:44 +0200

    first commit

---
+++ 1_flask_restful.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Item(Resource):
+    def get(self, name):
+        return {"Hello, " + 'item': name}
+
+api.add_resource(Item, '/item/<string:name>')
+
+
